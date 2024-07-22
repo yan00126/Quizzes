b23a685236898b030c67c6c01d84df4e27b5e48c
FeiYan <yan00126@algonquinlive.com>
2024-07-22 13:35:58 -0400

    Static Member,Constants, Enum and Exception Handling

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
-﻿internal class Program
+﻿using System.ComponentModel;
+using Quizzes;
+
+internal class Program
 {
+  enum Action {
+    Add = 1, // 1
+    Play, // 2
+    Exit // 3
+  }
+
+  // private static Quiz quiz = new Quiz(Helper.GetAvailableQuestions());
+  private static Quiz quiz = new Quiz([]);
+
   private static void Main(string[] args)
   {
     bool isActive = true;
@@ -13,6 +25,51 @@
       Console.Write("Enter an action: ");
       
       bool isValid = int.TryParse(Console.ReadLine(), out int action);
+    
+      switch ((Action)action)
+      {
+        case Action.Add:
+          //adding
+          Add();
+          break;
+        case Action.Play:
+          try 
+          {
+          //
+            quiz.PlayQuiz();
+          } 
+          catch (Exception e) 
+          {
+            Console.WriteLine(e.Message);
+          }
+          break;
+        case Action.Exit:
+          //
+          break;
+        default:
+          Console.WriteLine("Not a valid response.");
+          break;
+      }
+
     }
+  }
+
+  private static void Add()
+  {
+    string? text = null;
+    string? answer = null;
+
+    while (string.IsNullOrWhiteSpace(text))
+    {
+      Console.WriteLine("Enter the Question: ");
+      text = Console.ReadLine();
+    }
+
+    while (string.IsNullOrWhiteSpace(answer))
+    {
+      Console.WriteLine("Enter the Question: ");
+      answer = Console.ReadLine();
+    }
+    quiz.AddQuestion(text, answer);
   }
 }
---
+++ Quiz.cs
@@ -0,0 +1,56 @@
+﻿namespace Quizzes;
+
+public class Quiz
+{
+    // fields
+    private List<Question> questions;
+
+    //constructor
+    public Quiz (List<Question> questions)
+    {
+        this.questions = questions;
+
+    }
+
+    //methods
+
+    public void AddQuestion (string text, string answer)
+    {
+        questions.Add(new Question(text, answer));
+    }
+
+    public void PlayQuiz ()
+    {
+        if (questions.Count == 0)
+        {
+            throw new Exception("No Question in the quiz.");
+        }
+        // Randomiz the order of questions
+        var shuffled = Helper.Shuffle(questions);
+
+        // play through all questions one at a time
+        foreach (var question in shuffled)
+        {
+
+        // asking user the questions
+            Console.WriteLine(question.Text);
+
+        // check user's answers
+            string? response = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(response) && question.CheckAnswer(response))
+            {
+        // responding
+                //is correct
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                // incorrect
+                Console.WriteLine($"Incorrect. Thre correct answer is: {question.Answer}");
+
+            }
+
+
+        }
+    }
+}
